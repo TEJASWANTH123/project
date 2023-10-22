77c12200ecf1949a5788a617153f177a3151c0e2
TTejaswanth <tejaswanth.t20@iiits.in>
2023-10-22 21:25:28 +0530

    upload

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import { authRoutes, todoRoutes } from './routes/index';
+const express = require('express');
+const cors = require('cors');
+const { authRoutes, todoRoutes } = require('./routes/index');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-import session from 'express-session';
+const session = require('express-session');
+const multer = require('multer');
+const path = require('path'); // To work with file paths
+
 
 // import multer, { Multer } from 'multer'; // Comment out or remove this line
 
@@ -13,17 +16,35 @@ app.use(cors());
 app.use(express.json());
 
 app.use(
-    session({
-      secret: 'your-secret-key', // Replace with a secure secret key
-      resave: false,
-      saveUninitialized: false,
-    })
-  );
+  session({
+    secret: 'your-secret-key', // Replace with a secure secret key
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use('/api', todoRoutes);
 app.use('/auth', authRoutes);
 
 // Set up the Swagger UI route using the updated Swagger JSON file
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const storage: multer.StorageEngine = multer.diskStorage({
+  destination: (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) => {
+    cb(null, 'uploads/'); // The 'uploads' folder is the destination for file uploads
+  },
+  filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  },
+});
+
+
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('file'), (req: Request, res: Response) => {
+  // res.json({ message: 'File uploaded successfully' });
+});
 
 const PORT = 3000;
 
